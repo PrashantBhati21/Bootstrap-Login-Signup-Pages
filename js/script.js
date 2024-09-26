aa69a3cd83f997dd502dd0bb6bcb7d371a794e58
Prashant Bhati <bhati.prashant543@gmail.com>
2024-09-26 10:41:56 -0500

    forgot password and verify otp pages added

---
+++ js/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const inputs = document.querySelectorAll(".form-control");
+
+  // Loop through each input element
+  inputs.forEach((input, index) => {
+
+    // Event listener for when user types in the input field
+
+    input.addEventListener("input", () => {
+      if (input.value.length === 1 && index < inputs.length - 1) {
+
+        // Move focus to the next input if the current one is filled
+
+        inputs[index + 1].focus();
+      }
+    });
+
+    // Event listener for handling 'Backspace' key
+    
+    input.addEventListener("keydown", (e) => {
+      if (e.key === "Backspace" && input.value === "" && index > 0) {
+
+        // Move focus to the previous input if 'Backspace' is pressed and current input is empty
+
+        inputs[index - 1].focus();
+      }
+    });
+  });
+});
+const otpSent = () => {
+  alert("OTP verified successfully");
+};
