75548cf7f043f3977f5fcc6b3bd81c7027227e72
Prashant Bhati <bhati.prashant543@gmail.com>
2024-09-26 11:40:57 -0500

    file path updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Login Page Link- https://prashantbhati21.github.io/Bootstrap-Login-Signup-Pages/Loginform.html
 
 Signup Page Link - https://prashantbhati21.github.io/Bootstrap-Login-Signup-Pages/Signupform.html
+
+
--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   const inputs = document.querySelectorAll(".form-control");
 
   // Loop through each input element
   inputs.forEach((input, index) => {
-
     // Event listener for when user types in the input field
 
     input.addEventListener("input", () => {
       if (input.value.length === 1 && index < inputs.length - 1) {
-
         // Move focus to the next input if the current one is filled
 
         inputs[index + 1].focus();
@@ -17,10 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Event listener for handling 'Backspace' key
-    
+
     input.addEventListener("keydown", (e) => {
       if (e.key === "Backspace" && input.value === "" && index > 0) {
-
         // Move focus to the previous input if 'Backspace' is pressed and current input is empty
 
         inputs[index - 1].focus();
